4d480f8cb61cb6e250a5af3b1e855c7bb20c8263
Andrei Gavrila <Andrei.Gavrila@catalysts.cc>
2019-03-18 17:39:41 +0200

    fail intentionally

--- src/test/java/g936/AppTest.java
+++ src/test/java/g936/AppTest.java
@@ -3,12 +3,13 @@ package g936;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 
 public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        fail();
     }
 }
